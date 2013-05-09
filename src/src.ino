3d3498cf96f673c24e79c342f6a44c814cf7a69f
Kevin Loney <kevin.loney@gmail.com>
2013-05-08 21:26:26 -0600

    First working version of autonomous driving code

--- src/src.ino
+++ src/src.ino
@@ -1,137 +1,156 @@
 #include <NewPing.h>
 #include <Servo.h>
- 
+
+
+
 const int ENA_PIN = 6; //grey <- RIGHT
 const int ENB_PIN = 3; //orange <-LEFT
 
-const int AIN1_PIN = 10; //purple
-const int AIN2_PIN = 5; //blue
-const int BIN1_PIN = 2; //green
-const int BIN2_PIN = 4; //yellow
+const int AIN1_PIN = 4; //purple
+const int AIN2_PIN = 7; //blue
+const int BIN1_PIN = 8; //green
+const int BIN2_PIN = 9; //yellow
 
-const int SERVO_PIN = 8; //Servo
+const int SERVO_PIN = 5; //Servo
 
-const int TRIGGER_PIN = 12;  // Yellow -Arduino pin tied to trigger pin on ping sensor.
-const int ECHO_PIN = 11;  // Orange - Arduino pin tied to echo pin on ping sensor.
+const int TRIGGER_PIN = 13;  // Yellow -Arduino pin tied to trigger pin on ping sensor.
+const int ECHO_PIN = 12;  // Orange - Arduino pin tied to echo pin on ping sensor.
 
-const int MAX_DISTANCE = 90; // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+const int MAX_DISTANCE = 120; // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+const int TURN_DISTANCE = 15;
+const int FUZZ_DISTANCE = 3;
 const int SERVO_CENTER = 84;
 const int MAX_SERVO_SWEEP = 70;
 
-#define SAMPLES 20
+const int MAX_SPEED = 200;
+const int MIN_SPEED = 90;
 
-#define MODE_START 0
-#define MODE_AUTO 1
-#define MODE_REMOTE 2
+const int MAX_TURN_TIME = 1000;
+const int MIN_TURN_TIME = 200;
 
-int mode = MODE_START;
+#define MODE_AUTO 0
+#define MODE_REMOTE 1
+
+#define turnLeft(speed) motorControl((speed), -(speed))
+#define turnRight(speed) motorControl(-(speed), (speed))
+#define forward(speed) motorControl((speed), (speed))
+#define reverse(speed) motorControl(-(speed), -(speed))
+#define stop() motorControl(0, 0)
+
+int mode = MODE_AUTO;
 
 Servo servo;        // servo objects
 NewPing srf06(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
- 
 void setup() {
   Serial.begin(9600);
-
-  // Configure h0bridge cotrol pins
-  digitalWrite(ENA_PIN, LOW); //Enable Right Motor
-  digitalWrite(ENB_PIN, LOW); //Enable Left Motor
-  analogWrite(AIN2_PIN, LOW); //<- On for reverse
-  analogWrite(AIN1_PIN, LOW); //<- On for Forward
-  analogWrite(BIN2_PIN, LOW); //<- On for reverse
-  analogWrite(BIN1_PIN, LOW); //<- On for Forward    
-
+  
+  // Initial motor configuration
+  pinMode(AIN1_PIN, OUTPUT);
+  pinMode(AIN2_PIN, OUTPUT);
+  pinMode(BIN1_PIN, OUTPUT);
+  pinMode(BIN2_PIN, OUTPUT);
+  stop();
+  
+  // Center the servo
   servo.attach(SERVO_PIN);
   servo.write(SERVO_CENTER); // set servo to center pois
 }
  
 void loop() {
     switch(mode) {
-        case MODE_START:
-          return;
-        case MODE_AUTO: break;
+        case MODE_AUTO: modeAutonomous(); break;
         case MODE_REMOTE: break;
         default: break;
     }
     return;
-
-/*
-  fwdDistance =  srf06.ping_cm();
-//  delay(100);
-  if (fwdDistance > 30 || fwdDistance == 0)
-  {
-    forward();
-    Serial.println("forward");
-  }
-  else
-  {
-    stop();
-    MrServo.write(ServoCenter + ServoOffset);  // move servo left
-    delay(500);
-    leftDistance = srf06.ping_cm();
-    delay(100);
-    MrServo.write(ServoCenter - ServoOffset);    // move servo right
-    delay(500);
-    rightDistance = srf06.ping_cm();
-    delay(100);
-    MrServo.write(ServoCenter);    // move servo to center
-    delay(50);
-    leftDistance = leftDistance == 0?0x7FFF:leftDistance;
-    rightDistance = rightDistance == 0?0x7FFF:rightDistance;
-    compareDistance();
- 
-  }     
-*/
-}
- 
-void compareDistance()
-{
-    /*
-  Serial.print(leftDistance);
-  Serial.print(" ");
-  Serial.println(rightDistance);
-  if (leftDistance>rightDistance) //if left is less obstructed
-  {
-    turnleft();
-    Serial.println("left");
-    delay(800);
-  }
-  else if (rightDistance>leftDistance) //if right is less obstructed
-  {
-    turnright();
-    Serial.println("right");
-    
-    delay(800);
-  }
-  else //if (rightDistance = leftDistance)
-  {
-    turnleft();
-     Serial.println("no good option, go left");
-    delay(1500);
-  }
-  */
 }
 
-void turnLeft(int speed) {
-    motorControl(speed, -speed);
-}
+int turnRandom = 0;
+unsigned long turnAttempts = 0;
 
-void turnRight(int speed) {
-    motorControl(-speed, speed);
-}
+unsigned long modeDistance = 0;
+unsigned long nextDistance = 0;
 
-void forward(int speed) {
-    motorControl(speed, speed);
-}
+void modeAutonomous() {
+    int distance = srf06.ping_cm();
+    if(distance > 0) {
+        int speed = map(distance, 0, MAX_DISTANCE, MIN_SPEED, MAX_SPEED);
 
-void halt() {
-    motorControl(0, 0);
+        if(distance > TURN_DISTANCE) {
+            forward(speed);
+        }
+        else {
+            reverse(MAX_SPEED);
+            delay(100);
+            stop();
+
+            int theta = constrain((int)floor(180.0 * atan2(9, distance) / M_PI + 0.5), 0, MAX_SERVO_SWEEP);
+
+            servo.write(SERVO_CENTER + theta);
+            delay(100);
+            int lDist = srf06.ping_cm();
+
+            servo.write(SERVO_CENTER - theta);
+            delay(100);
+            int rDist = srf06.ping_cm();
+
+            if(abs(lDist - rDist) < FUZZ_DISTANCE) {
+                if(turnRandom == 0) {
+                    turnRandom = ((random() & 1) << 1) - 1;
+                }
+                if(turnRandom < 0) {
+                    turnLeft(MAX_SPEED);
+                }
+                else {
+                    turnRight(MAX_SPEED);
+                }
+            }
+            else if(lDist > rDist || lDist == 0) {
+                turnLeft(MAX_SPEED);
+            }
+            else {
+               turnRight(MAX_SPEED);
+            }
+            delay(map(turnAttempts, 0, 10, MIN_TURN_TIME, MAX_TURN_TIME));
+            ++turnAttempts;
+
+            servo.write(SERVO_CENTER);
+        }
+    }
+    else {
+        turnRandom = 0;
+        turnAttempts = 0;
+        forward(MAX_SPEED);
+    }
 }
 
 void motorControl(int rSpeed, int lSpeed) {
     rSpeed = constrain(rSpeed, -255, 255);
     lSpeed = constrain(lSpeed, -255, 255);
 
+    analogWrite(ENA_PIN, abs(rSpeed));
+    analogWrite(ENB_PIN, abs(lSpeed));
+
+    if(rSpeed > 0) {
+        digitalWrite(AIN2_PIN, LOW); //<- On for reverse
+        digitalWrite(AIN1_PIN, HIGH); //<- On for Forward
+    }
+    else {
+        digitalWrite(AIN2_PIN, HIGH); //<- On for reverse
+        digitalWrite(AIN1_PIN, LOW); //<- On for Forward
+    }
+
+    if(lSpeed > 0) {
+        digitalWrite(BIN2_PIN, HIGH); //<- On for reverse
+        digitalWrite(BIN1_PIN, LOW); //<- On for Forward    
+    }
+    else {
+        digitalWrite(BIN2_PIN, LOW); //<- On for reverse
+        digitalWrite(BIN1_PIN, HIGH); //<- On for Forward    
+    }
+
+/*
     digitalWrite(ENA_PIN, rSpeed == 0 ? LOW : HIGH);
     digitalWrite(ENB_PIN, lSpeed == 0 ? LOW : HIGH);
 
@@ -152,4 +171,5 @@ void motorControl(int rSpeed, int lSpeed) {
         analogWrite(BIN2_PIN, LOW); //<- On for reverse
         analogWrite(BIN1_PIN, -lSpeed); //<- On for Forward    
     }
+*/
 }
